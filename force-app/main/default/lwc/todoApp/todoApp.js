91ef7dcee3ebbde5af7b85e9a76ae11fa16dc08d
iamprashu <iamprashu.gs@gmail.com>
2026-03-27 10:04:51 +0530

    Updated Components

--- force-app/main/default/lwc/todoApp/todoApp.js
+++ force-app/main/default/lwc/todoApp/todoApp.js
@@ -1,18 +1,37 @@
 import { LightningElement, track } from 'lwc';
 
 export default class TodoApp extends LightningElement {
+    num = 1;
     @track tasks = [];
 
+    connectedCallback() {
+        const storedTasks = localStorage.getItem('tasks');
+        this.tasks = storedTasks ? JSON.parse(storedTasks) : [];
+    }
+
     addTask() {
-    const inputElement = this.template.querySelector('.taskInput');
-    const data = inputElement.value;
+        const inputElement = this.template.querySelector('.taskInput');
+        const data = inputElement.value;
 
         if (data) {
             this.tasks = [
                 ...this.tasks,
-                { id: Date.now(), name: data }
+                { id: num+1, name: data }
             ];
+
             inputElement.value = '';
+
+            localStorage.setItem('tasks', JSON.stringify(this.tasks));
         }
+    }
+
+    removeTask(event) {
+        const rm = Number(event.currentTarget.dataset.id);
+
+        this.tasks = this.tasks.filter(
+            eachElement => eachElement.id !== rm
+        );
+
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
     }
 }
