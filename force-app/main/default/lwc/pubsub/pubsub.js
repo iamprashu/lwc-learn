3afe0edd0a2645d3716faa6f48fdbc889094c273
iamprashu <iamprashu.gs@gmail.com>
2026-03-30 11:30:13 +0530

    Debugging

--- force-app/main/default/lwc/pubsub/pubsub.js
+++ force-app/main/default/lwc/pubsub/pubsub.js
@@ -5,18 +5,23 @@ const registerListener = (eventName, callback) => {
         events[eventName] = [];
     }
     events[eventName].push(callback);
+    console.log(events);
 };
 
 const unregisterListener = (eventName, callback) => {
     if (events[eventName]) {
         events[eventName] = events[eventName].filter(cb => cb !== callback);
     }
+    console.log(events);
+
 };
 
 const fireEvent = (eventName, payload) => {
     if (events[eventName]) {
         events[eventName].forEach(callback => callback(payload));
     }
+    console.log(events);
+
 };
 
 export { registerListener, unregisterListener, fireEvent };
